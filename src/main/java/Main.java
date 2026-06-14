b7587af227299d8b97cbae0e21bb7d5189823a72
Krishna Aniket Tarai <2405507@kiit.ac.in>
2026-06-14 12:40:10 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,17 +5,19 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
 
+        while (true) {
             System.out.print("$ ");
 
             String command = scanner.nextLine();
+
             if (command.equals("exit")) {
                 break;
+            } else if (command.startsWith("echo ")) {
+                System.out.println(command.substring(5));
+            } else {
+                System.out.println(command + ": command not found");
             }
-
-            System.out.println(command + ": command not found");
         }
     }
-
 }
